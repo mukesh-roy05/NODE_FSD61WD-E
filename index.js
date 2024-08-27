ffef3c61ea635acaa85cd7de79ea0aa2e70490c3
Mukesh Roy <mukesh.raj60@gmail.com>
2024-08-27 12:42:26 +0530

    Updated file

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  console.log(request);
-  console.log(request.body);
-  response.end("First Node Server!\n");
+  const { url, method } = request;
+  if (url === "/") {
+    if (method === "GET") {
+      return response.end("GET World");
+    } else if (method === "POST") {
+      return response.end("POST World");
+    }
+  } else if (url === "/test") {
+    return response.end("Hello World");
+  } else return response.end("Endpoint not found");
 });
 
 // starts a simple http server locally on port 3000
